9f646ab261d416c68869ec461011f56a613d3a74
Mike Christensen <mike_christensen@btinternet.com>
2019-10-31 16:53:25 +0000

    set zsh as default shell, use oh-my-zsh

--- zsh/zshrc.symlink.sh
+++ zsh/zshrc.symlink.sh
@@ -1,7 +1,12 @@
 # shortcut to this dotfiles path is $ZSH
-export ZSH=$HOME/.dotfiles
+export ZSH=$HOME/.oh-my-zsh
 
-# load the path files
-for file in $(find $ZSH -mindepth 2 -maxdepth 2 -not -path '*.git*' -name path.zsh); do
+# source the path.zsh files first
+for file in $(find $HOME/.dotfiles -mindepth 2 -maxdepth 2 -not -path '*.git*' -name path.zsh); do
+    source $file
+done
+
+# source all other zsh files
+for file in $(find $HOME/.dotfiles -mindepth 2 -maxdepth 2 -not -path '*.git*' -name '*.zsh' | grep -v path.zsh); do
     source $file
 done
